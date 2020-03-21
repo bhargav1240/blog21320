2e0d162bd29a821b3fc633348d10da7f0b8a13f3
bhargavrao1240 <bhavu92@gmail.com>
2020-03-21 16:01:07 +0530

    change login page

---
+++ resources/views/layouts/partials/scripts.blade.php
@@ -0,0 +1,8 @@
+<!-- jQuery -->
+<script src="{{ asset('admin_lte/jquery/jquery.min.js') }}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{ asset('admin_lte/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<!-- AdminLTE App -->
+<script src="{{ asset('admin_lte/dist/js/adminlte.min.js') }}"></script>
+
+<script src="{{ asset('js/app.js') }}"></script>
